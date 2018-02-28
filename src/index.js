01864065cf3818920b0678c9d941a3eae28eb075
NSpeko <nonstoper42@gmail.com>
2018-02-28 11:04:30 +0300

    Feature: Implemented(those humongous test file made webstorm get sick)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+    let result=[];
+    array=array.sort((num1,num2)=>{return num1-num2;});
+    result[0]=0;
+    for(let i=1;i<array.length;i++)result[i]=array[i]===array[i-1]+1?result[i-1]+1:1;
+    console.log(Math.max.apply({},result));
+    return Math.max.apply({},result);
+
 }
