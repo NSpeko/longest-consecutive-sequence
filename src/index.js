0e3c8587a5b0c654f89eedbe4a0d19bf7b0aa466
NSpeko <nonstoper42@gmail.com>
2018-03-09 18:46:24 +0300

    Fix: got rid of sorting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
-    let result=[];
-    array=array.sort((num1,num2)=>{return num1-num2;});
-    result[0]=0;
-    for(let i=1;i<array.length;i++)result[i]=array[i]===array[i-1]+1?result[i-1]+1:1;
-    console.log(Math.max.apply({},result));
-    return Math.max.apply({},result);
+    let lastNo = Infinity;
+    let maxSequence = 0;
+    let currSequence = 0;
+    console.log(array.length);
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === lastNo + 1) {
+            currSequence++;
+            maxSequence = Math.max(maxSequence,currSequence);
+        }else currSequence = 1;
+        lastNo = array[i];
+    }
+    return Math.max(maxSequence, currSequence);
 
 }
